21fa546a9ef63191e159987917b1891eed70f24b
LucHeart <luc@luc.cat>
2026-09-10 20:12:37 +0200

    refactor: move StringUtils out of Ui.Utils

--- Desktop/Utils/StringUtils.cs
+++ Desktop/Utils/StringUtils.cs
@@ -1,7 +1,8 @@
-﻿namespace OpenShock.Desktop.Ui.Utils;
+﻿using OpenShock.SDK.CSharp.Models;
+
+namespace OpenShock.Desktop.Utils;
 
 public static class StringUtils
 {
     public static string Truncate(this string input, int maxLength) => input[..Math.Min(maxLength, input.Length)];
-    
 }
